cff6a7ed5a810f843bc9ae6b853c092ff2d72952
BekirGeris <48486272+BekirGeris@users.noreply.github.com>
2023-10-03 22:47:47 +0300

    Initial Commit

---
+++ CircularShapeAnimationApp/CircularShapeAnimationAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CircularShapeAnimationAppApp.swift
+//  CircularShapeAnimationApp
+//
+//  Created by Bekir Geriş on 3.10.2023.
+//
+
+import SwiftUI
+
+@main
+struct CircularShapeAnimationAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
